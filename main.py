579959cc771f3ca818745967d4011684d08f049f
fjr-joestar <fjr.rodriguez40@gmail.com>
2026-05-22 11:14:24 -0500

    Calculator Project

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
+import tkinter
+
 beginnings = "Hello World"
 print(beginnings)
 
 savings = 406948
 interest = 0.30
-print(savings * interest)
+print (savings + savings * interest)
+
+
+
+window = tkinter.Tk()
+label = tkinter.Label()
+
+window.title("Savings")
+
+window.update()
+window.mainloop()
